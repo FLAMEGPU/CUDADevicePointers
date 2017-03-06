39e90776e868b9323afdf3b45edd774f687d80d5
mondus <p.richmond@sheffield.ac.uk>
2017-03-06 12:17:11 +0000

    added some more tests

--- CUDAFunctionPointers/kernel.cu
+++ CUDAFunctionPointers/kernel.cu
@@ -19,6 +19,16 @@ typedef void(*func_ptr)();
 //symbol addresses do not exist within the same unified address space like a host accessible device pointer!
 __device__ func_ptr ds_f1_ptr = func1;
 
+inline func_ptr get_function(func_ptr d_symbol){
+	func_ptr h_ptr;
+	
+	cudaMemcpyFromSymbol(&h_ptr, d_symbol, sizeof(func_ptr));
+
+	return h_ptr;
+}
+
+
+
 
 //example kenrel
 __global__ void kernel(func_ptr f)
@@ -36,9 +46,10 @@ __global__ void kernel(func_ptr f)
 
 }
 
-int main()
+void test1()
 {
 	
+	printf("Test1\n");
 	//host pointer
 	func_ptr h_f1_ptr;
 
@@ -58,5 +69,73 @@ int main()
 	//sync to ensure printf outputs
 	cudaDeviceSynchronize();
 
-    return 0;
+}
+
+
+void test2()
+{
+	printf("Test2\n");
+
+	//host pointer
+	func_ptr h_f1_ptr;
+
+	const void *temp;
+
+	temp = ds_f1_ptr;
+
+	//get ds_f1_ptr as a device address rather than a device symbol address
+	cudaMemcpyFromSymbol(&h_f1_ptr, temp, sizeof(func_ptr));
+
+	//print symbol address on host (not accessible from host)
+	printf("Host address of temp (symbol) is 0x%08X\n", temp);
+
+	//print the host copy of the address copied form symbol
+	printf("Host address of h_f1_ptr is 0x%08X\n", h_f1_ptr);
+
+
+	//call kernel
+	kernel << <1, 1 >> >(h_f1_ptr);
+
+	//sync to ensure printf outputs
+	cudaDeviceSynchronize();
+
+}
+
+void test3()
+{
+	printf("Test3\n");
+
+	//host pointer
+	func_ptr h_f1_ptr;
+
+	h_f1_ptr = get_function(ds_f1_ptr);
+
+	//print symbol address on host (not accessible from host)
+	printf("Host address of ds_f1_ptr (symbol) is 0x%08X\n", ds_f1_ptr);
+
+	//print the host copy of the address copied form symbol
+	printf("Host address of h_f1_ptr is 0x%08X\n", h_f1_ptr);
+
+
+	//call kernel
+	kernel << <1, 1 >> >(h_f1_ptr);
+
+	//sync to ensure printf outputs
+	cudaDeviceSynchronize();
+
+}
+
+int main()
+{
+
+	//works
+	test1();
+
+	//fails
+	//test2();
+
+
+	test3();
+
+	return 0;
 }
